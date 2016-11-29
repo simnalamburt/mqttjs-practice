f27806349a8eb534a29014fbb6b39a70c3a9350a
Hyeon Kim <simnalamburt@gmail.com>
2016-11-30 01:41:35 +0900

    Rename: client/ -> public/

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
