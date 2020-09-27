995d6641174781d1294b4d3deab0aaaa5b2bc741
Hyeon Kim <simnalamburt@gmail.com>
2020-09-28 02:51:00 +0900

    Remove redundant webpack plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,16 +1,9 @@
 'use strict'
 
 const path = require('path')
-const webpack = require('webpack')
-const merge = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CompressionPlugin = require('compression-webpack-plugin')
-const zopfli = require('@gfx/zopfli')
 
-//
-// Common configs
-//
-const commonConfigs = {
+module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
@@ -26,27 +19,3 @@ const commonConfigs = {
   },
   plugins: [new MiniCssExtractPlugin({ filename: 'bundle.css' })]
 }
-
-// Development-mode configs
-const dev = {
-  devtool: 'inline-source-map'
-}
-
-// Production-mode configs
-const prod = {
-  plugins: [
-    new webpack.LoaderOptionsPlugin({ minimize: true, debug: false }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new CompressionPlugin({
-      test: /\.(?:css|js|svg|eot|ttf)$/,
-      algorithm: zopfli.gzip,
-      minRatio: 1,
-      compressionOptions: { numiterations: 15 }
-    })
-  ]
-}
-
-module.exports = (_, { mode }) =>
-  merge(commonConfigs, mode === 'production' ? prod : dev)
