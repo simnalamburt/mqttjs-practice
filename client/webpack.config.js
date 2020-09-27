47d87cefd33b71d5b5ef426fc045e3a97ed04c31
Hyeon Kim <simnalamburt@gmail.com>
2020-09-28 03:59:50 +0900

    Use TypeScript

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,8 +4,17 @@ const PnpWebpackPlugin = require('pnp-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
