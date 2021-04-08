e1807e2626132e5fee107a48352bbc5c5413fccc
Hyeon Kim <simnalamburt@gmail.com>
2021-04-08 19:47:10 +0900

    Add polyfills

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,19 @@
 'use strict'
 
+const { ProvidePlugin } = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.ts',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
+
+    // Required due to 'mqtt' module
+    fallback: {
+      buffer: require.resolve('buffer/'),
+      events: require.resolve('events/'),
+      process: require.resolve('process/'),
+    },
   },
   module: {
     rules: [
@@ -20,5 +28,13 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+
+    // Required due to 'mqtt' module
+    new ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: ['process'],
+    }),
+  ],
 }
